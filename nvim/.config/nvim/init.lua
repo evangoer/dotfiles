8b1a1eef636b3062d0fd187ff84b2cd1680bed10
Evan Goer <evan@goer.org>
2026-02-21 23:11:43 -0800

    Add gitsigns with leader shortcuts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -71,6 +71,7 @@ Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' }) -- Syntax high
 Plug('nvim-treesitter/nvim-treesitter-textobjects') 
 Plug('ibhagwan/fzf-lua') -- Fuzzy finding
 Plug('stevearc/conform.nvim') -- Formatting and linting
+Plug('lewis6991/gitsigns.nvim')
 vim.call('plug#end')
 
 vim.cmd('colorscheme kanagawa-dragon')
@@ -146,4 +147,32 @@ require('conform').setup({
     timeout_ms = 500,
     lsp_fallback = true,
   },
+})
+
+require('gitsigns').setup({
+  on_attach = function(bufnr)
+    local gs = require('gitsigns')
+    local function map(mode, l, r, desc)
+      vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+    end
+
+    -- Navigation
+    map('n', ']c', function() gs.nav_hunk('next') end, 'Next hunk')
+    map('n', '[c', function() gs.nav_hunk('prev') end, 'Prev hunk')
+
+    -- Stage / undo / reset
+    map('n', '<leader>hs', gs.stage_hunk, 'Stage hunk')
+    map('n', '<leader>hr', gs.reset_hunk, 'Reset hunk')
+    map('v', '<leader>hs', function() gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, 'Stage selection')
+    map('v', '<leader>hr', function() gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, 'Reset selection')
+    map('n', '<leader>hu', gs.undo_stage_hunk, 'Undo stage hunk')
+    map('n', '<leader>hS', gs.stage_buffer, 'Stage buffer')
+    map('n', '<leader>hR', gs.reset_buffer, 'Reset buffer')
+
+    -- Preview / blame / diff
+    map('n', '<leader>hp', gs.preview_hunk, 'Preview hunk')
+    map('n', '<leader>hb', function() gs.blame_line({ full = true }) end, 'Blame line')
+    map('n', '<leader>hB', gs.toggle_current_line_blame, 'Toggle line blame')
+    map('n', '<leader>hd', gs.diffthis, 'Diff against index')
+  end,
 })
